467a7e5331b9735f7921f0dd44bcb0262f932687
Badu <badu@dcard.cc>
2018-08-01 15:41:28 +0800

    test NoStatusBar, NoActionBar, NoStatusBArAndActionBar

--- app/src/main/java/com/badu/statusbarsample/MainActivity.kt
+++ app/src/main/java/com/badu/statusbarsample/MainActivity.kt
@@ -2,11 +2,18 @@ package com.badu.statusbarsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
